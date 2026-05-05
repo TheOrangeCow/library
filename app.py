039c68c24aa49103d95b8dd77ae488fbadb5c935
TheOrangeCow <138312355+TheOrangeCow@users.noreply.github.com>
2026-05-05 20:25:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import eventlet
-eventlet.monkey_patch()
+from gevent import monkey
+monkey.patch_all()
 
 from flask import  session, render_template, redirect, url_for
 from core import app, socketio
